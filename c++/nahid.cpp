f3be2631ab65d0bd2cd911b03054852160aad4ab
nirob5151 <masudurrahmannirob@gmail.com>
2023-09-23 02:28:59 +0600

    word lenth

--- c++/nahid.cpp
+++ c++/nahid.cpp
@@ -10,3 +10,23 @@ class massage
     virtual void sendmassage()=0;
 };
 class king:public massage
+{
+    cout<<"Hi i am king"<<endl;
+
+};
+class nirob:public massage
+{
+    cout<<"THIS IS NIROB";
+};
+int main()
+{
+    massage *m;
+    king k;
+    nirob n;
+    m->call();
+    m=&k;
+    m->sendmassage();
+    m=&n;
+    m->sendmassage();
+    return 0;
+}
---
+++ c++/word lenth.cpp
@@ -0,0 +1,15 @@
+# include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+   // int n;//
+    cin>>s;
+    int i=0,lenth=0;
+    while (s[i]!='\0')
+    {
+        i++;
+        lenth++;
+    }
+    cout<<"Lenth is : "<<lenth;
+}
