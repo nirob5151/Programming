80b920bed07c8970fbbfc20a547e80779515bfa7
nirob5151 <masudurrahmannirob@gmail.com>
2025-01-05 04:18:42 +0600

    learning

--- Paython/operator.py
+++ Paython/operator.py
@@ -1,3 +1,17 @@
-print(5+9);
-print(5+5-10*1/10);
+#print(5+9);
+#print(5+5-10*1/10);
 # new sikhtaci vaia ra
+#a=int(input())
+#b=int(input())
+#print(a+b);
+
+w=int(input())
+x=int(input())
+y=int(input())
+z=int(input())
+print(
+    (w+x)*(y*z)
+)
+print(w/x)
+print(y//z)
+print(x%z)
